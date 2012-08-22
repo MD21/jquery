af52bc8bcca55d276bca42be22f1c7297c675a0e
abhinav srivastav <abhinav.srivastav@vinsol.com>
2012-08-22 17:10:20 +0530

    Update jquery4/js/inputHint.js

--- jquery4/js/inputHint.js
+++ jquery4/js/inputHint.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
 
   //Set the value of the search input to the text of the label element
-  $("input.input_text").attr("value",$("label").text());
+  $("input.input_text").val($("label").text());
 
   //Add a class of "hint" to the search input
   console.log($("input.input_text").addClass("hint"));
@@ -17,7 +17,7 @@ $(document).ready(function(){
 
   //Bind a blur event to the search input that restores the hint text and "hint" class if no search text was entered
   $("input.input_text").bind("blur",function(){
-  $("input.input_text").attr("value",label_detach.text());
+  $("input.input_text").val(label_detach.text());
   });
 
 });
