e8f5cdd95f6bb82d1527a6a806d9354dfdcc4506
abhinav srivastav <abhinav.srivastav@vinsol.com>
2012-08-22 18:25:02 +0530

    Update jquery3/js/sandbox.js

--- jquery3/js/sandbox.js
+++ jquery3/js/sandbox.js
@@ -1,24 +1,22 @@
 $(document).ready(function ()
 {
-  
+
   //Add five new list items to the end of the unordered list #myList.
   for(var add_5 = 0 ; add_5 < 5 ; add_5++)
-  $("ul#myList").append("<li> new list item "+add_5+"</li>");
-
+  $("ul#myList ").html($("ul#myList").html()+'<li>new list item '+add_5+'</li>');
+ 
   //Remove the odd list items
-  
   $("li:odd").detach();
 
   //Add another h2 and another paragraph to the last div.module
-  $("div").eq($("div").length-1).append("<h2>new h2 heading</h2>").append("<p>new paragraph<p>");
+  $("div").last().append("<h2>new h2 heading</h2>").append("<p>new paragraph<p>");
  
   //Add another option to the select element; give the option the value "Wednesday"
-  $("select").append("<option value = 'Wednessday'>Wednessday</option>");
+  $("select").append("<option ></option>").children().last().text("wednessday").attr("val","wednessday");
 
   //Add a new div.module to the page after the last one; put a copy of one of the existing images inside of it.
-  $("div").eq($("div").length-1).parent().append("<div>New Div </div>");
-  $("img").clone(true).appendTo($("div").eq($("div").length-1));
+  $("div").last().parent().append("<div></div>").children().last().text("new division");
+  $("img").first().clone(true).appendTo($("div").last());
 
 
 });
-
