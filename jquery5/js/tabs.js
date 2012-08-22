bedfb6d834ff7d59f15441efe22a8fee712557d1
abhinav srivastav <abhinav.srivastav@vinsol.com>
2012-08-22 19:42:22 +0530

    Update jquery5/js/tabs.js

--- jquery5/js/tabs.js
+++ jquery5/js/tabs.js
@@ -1,22 +1,22 @@
 $(document).ready(function(){
 
   //Hide all of the modules.
-    $(".module").hide();
-
   //Create an unordered list element before the first module.
-    console.log($("<ul>Unordered list</ul>").insertBefore($(".module").eq(0)));
-   
+
+  $(".module").hide().first().before("<ul></ul>").prev().text("unordered list");
     
-  // Iterate over the modules using $.fn.each. For each module, use the text of the h2 element as the text for a list item that you add to the 
-  //  unordered list element.
+   // Iterate over the modules using $.fn.each. For each module, use the text of the h2 element as the text for a list item that you add to the 
+   //  unordered list element.
    
-    $(".module").each(function(){
-     console.log($(this).before("<li id = added_list_item><h2> heading </h2>"));
-     });
+   $(".module").each(function(){
+      
+   console.log($(this).before("<li></li>").prev().attr("id","added_list_item").text($(this).children().first().text()));
+    
+    });
 
 
-  //Bind a click event to the list item that:
-    $("*#added_list_item").bind('click',function(){
+   //Bind a click event to the list item that:
+   $("*#added_list_item").bind('click',function(){
 
     //Shows the related module, and hides any other modules
      $(this).next(".module").show().siblings(".module").hide(); 
