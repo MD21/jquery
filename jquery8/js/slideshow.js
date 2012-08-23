01452547cdbed9554dd8f19c1e56eb8619172fee
abhinav srivastav <abhinav.srivastav@vinsol.com>
2012-08-23 17:04:57 +0530

    Update jquery8/js/slideshow.js

--- jquery8/js/slideshow.js
+++ jquery8/js/slideshow.js
@@ -4,21 +4,29 @@ $(document).ready(function(){
 
   $("body").prepend(slide_show);
 
+  var label_text = "total images are "+$("#slideshow li img").length+" and you are viewing image ";
+  var label = $("<label />").html(label_text);
+  var span = $("<span />").addClass("show_after");
+  $(label).append(span);
 
-  $("#slideshow").after('<label></label>').next("label").text("total images are "+$("#slideshow li img").length+
-                " and you are viewing image ").append("<span></span>").children().last().addClass("show_after");
+  $("#slideshow").after(label);
 
   $("#slideshow li").fadeOut();
 
   $("#slideshow li").ready(function slide_show (){
+    
+    var image_name1 = $("#slideshow li :first img").attr("alt");
+    var image_name2 = $("#slideshow li").eq(1).children("img").attr("alt");
+    var image_name3 = $("#slideshow li :last img").attr("alt");
 
-    console.log( $("span.show_after").html($("#slideshow li").first().children("img").attr("alt")));
+    $("span.show_after").html(image_name1);
+
     $("#slideshow li").first().fadeIn(700).delay(1000).fadeOut(700,function(){  
 
-      $("span.show_after").html($(this).next("li").children("img").attr("alt"));  
+      $("span.show_after").html(image_name2);  
       $(this).next("li").fadeIn(700).delay(1000).fadeOut(700,function(){
 
-        $("span.show_after").html($(this).next("li").children("img").attr("alt"));
+        $("span.show_after").html(image_name3);
         $(this).next("li").fadeIn(700).delay(1000).fadeOut(700,function (){
 
             setTimeout(slide_show(),10000)
