e400b8b1e0ecbffa3efa78788236bbfaffbed11b
abhinav srivastav <abhinav.srivastav@vinsol.com>
2012-08-23 16:38:18 +0530

    Update jquery6/js/blog.js

--- jquery6/js/blog.js
+++ jquery6/js/blog.js
@@ -1,15 +1,8 @@
 $(document).ready(function(){
 
-  var $next_time = false;
-  
   $("#blog h3").click(function(e){
     e.preventDefault();
-    if($next_time)
-    {
        $("#blog p.excerpt").slideUp();
        $(this).next("p.excerpt").slideDown();
-    }
-    else
-      $next_time = $(this).next("p.excerpt").slideDown();
-  });
+     });
 });
