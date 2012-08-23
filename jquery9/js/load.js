26fb6eccfd11162a06a9dd69c3c828ddeb1f6cec
abhinav srivastav <abhinav.srivastav@vinsol.com>
2012-08-23 11:28:03 +0530

    Update jquery9/js/load.js

--- jquery9/js/load.js
+++ jquery9/js/load.js
@@ -2,21 +2,20 @@ $(document).ready(function(){
 
   $("div#blog ul li h3").each(function(index){
 
-    $(this).after("<div id = post"+(index + 1)+"></div>");
+    $(this).after("<div></div>").next("div").attr( "id" , "post"+(index + 1) );
 
-    $(this).data($(this).children("a").attr("href"),$(this).next("div"));
+    $(this).data( $(this).children("a").attr("href") , $(this).next("div") );
 
   });
 
 
-  $("div#blog ul li h3").bind('click',function(e){
+  $("div#blog ul li h3").bind( 'click' , function(e){
     e.preventDefault();
 
-    $(this).data($(this).children("a").attr("href")).load('data/blog.html #'+$(this).next("div").attr("id"));
+    $(this).data( $(this).children("a").attr("href") ).load( 'data/blog.html #'+$(this).next("div").attr("id") );
   });
 
 });
-
 
 
 
