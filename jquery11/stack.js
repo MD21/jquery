b09963b5c0e5a6019d4c9ffd4be92659b98c8868
abhinav srivastav <abhinav.srivastav@vinsol.com>
2012-08-24 11:01:11 +0530

    Update jquery11/stack.js

--- jquery11/stack.js
+++ jquery11/stack.js
@@ -1,33 +1,23 @@
 
-
-  var count = 1;
-
   function add_item()
   {
-    var div = $("<div />").attr("id",count).text(count++);
-    $("div#container ").prepend(div);
+    var count = $("div#container div:first").text();
+    var div = $("<div />").text(++count);
+    $("div#container").prepend(div);
   }
 
   $(document).ready(function(){
-
-    $("div#container").delegate("div","click",function(){
-
-      var first_div = $("div#container div :first");
-
-      if($(this).attr("id") ==  $(first_div).attr("id") )
-      {  
-         if($("div#container div").length == 1)
-           count = 1;
-         else
-           count = $(this).attr("id");
-
-         $(this).siblings().css("background-color","orange");
-         $(first_div).detach();
-        
-      }
-      else
-      {
+   
+      $("div#container").delegate("div ","click",function(){
+    
         $(this).css("background-color","white").siblings().css("background-color","orange");
-      } 
- });
-});
+      }); 
+
+      $("div#container").delegate("div:first","click",function(){
+
+        $("div#container div :first").detach();
+        
+      });
+  
+  });
+
