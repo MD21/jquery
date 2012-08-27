aceab027be062fccb8e62c91ae201cae503f4d78
Abhinav Srivastav <abhinav@abhinav-HP-ProBook-4530s.(none)>
2012-08-27 14:43:50 +0530

    slideshow updated

--- jquery8/js/slideshow.js
+++ jquery8/js/slideshow.js
@@ -9,28 +9,6 @@ $(document).ready(function(){
   var span = $("<span />").addClass("show_after");
   $(label).append(span);
 
-  $("#slideshow").after(label);
-<<<<<<< HEAD
-
-  $("#slideshow li").hide();
-
-  function slideshow(a,img_no)
-  {
-    $(a).fadeIn(700).delay(1000).fadeOut(700);
-    $("span.show_after").text(img_no);
-  }
-
-  $(function(){	
-    var i = 0;
-    setInterval(function(){
-
-      slideshow($("#slideshow li ").eq(i++),i);
-
-      if(i == 3)
-       i = 0;
-    },2405);
-=======
-
   $("#slideshow li").hide();
 
   function slideshow(a,img_no)
@@ -40,18 +18,15 @@ $(document).ready(function(){
   }
 
   $(function(){  
-    var i = 0;
+    slideshow($("#slideshow li:first"),0);
+    var i = 1;
     setInterval(function(){
 
       slideshow($("#slideshow li ").eq(i++),i);
 
-<<<<<<< HEAD
->>>>>>> fb720b32f50edcc213e53ab28209ef3573610ce7
-=======
       if(i == 3)
        i = 0;
     },2405);
->>>>>>> 309e7ac8de37436968b957c52e2e00ebda02a712
   });
 
 });
