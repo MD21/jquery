a4dad0c9ac96b378898ff61b870fe633d361b8ac
Abhinav Srivastav <abhinav@abhinav-HP-ProBook-4530s.(none)>
2012-09-01 11:17:03 +0530

    added emp role part 2

---
+++ jq-special2-part2/emp_role.js
@@ -0,0 +1,85 @@
+$(document).ready(function() {
+		
+  $( "#employees li.ui-draggable" ).draggable({
+    appendTo: "body",
+    helper: "clone"
+  });
+	
+  $( "#roles ul li ul" ).droppable({
+    activeClass: "ui-state-default",
+    hoverClass: "ui-state-hover",
+    accept: ":not(.ui-sortable-helper)",
+    drop: function( event, ui ) {
+
+      var id = $(this).attr("id");
+      var name = ui.draggable.text();
+      var i = $(this).children("li:contains("+name+")").length;
+
+      if(i == 0 ){
+        $( "<li></li>" ).text( ui.draggable.text() ).appendTo( this );
+        add_todo(ui.draggable.text(),id);
+       }
+     }
+   });
+    
+
+
+    $("* img#show_hide").click(function(e){
+
+       var li_display = $(this).siblings("li");
+
+       if($(li_display).css('display') == 'none')
+       $(li_display).slideDown();
+       else
+       $(li_display).slideUp();
+      
+     });
+
+
+      function add_todo(name,id)
+      {
+
+        var emp_name = $("<div/>").attr("id","emp_name").text(name);
+        var todo_list = $("<div/>").attr("id","todo_box");
+        var add_img = $("<img/>").attr("src","images/add.png");
+        var init_text = $("<span/>").attr("id","add_todo").text("add todos for "+name+" here");
+        add_img.appendTo(todo_list);
+        init_text.appendTo(todo_list);
+
+        var li = $("<li></li>");
+        emp_name.appendTo(li);
+        todo_list.appendTo(li);
+
+        $(li).appendTo($("#todos ul>li>ul#"+id));
+   
+      }
+
+   $("#roles ul>li>ul").delegate("#roles ul>li>ul>li ","mouseover mouseout", function(event){
+    var li = $(this);
+    if (event.type == 'mouseover') {
+
+      $(li).css("background-color","grey");
+      var del = $("<img />").attr("src","images/delete.png");
+      del.prependTo($(li));
+
+      $(li).children("img").click(function(){
+        var del_li = confirm("Do u want to delete the Employee !");
+        if(del_li == true)
+        {
+
+          var name = $(li).text();
+          var todo_div = $("#todos div#emp_name:contains('"+name+"') ");
+          var todo_li = $(todo_div).parent();
+
+          $(li).detach();
+          $(todo_li).detach();
+
+         }
+     });
+    }
+    else{
+      $(this).css("background-color","white");
+      $(this).children("img").detach();
+    }
+  });
+});
---
+++ jq-special2-part2/emp_role_todo.js
@@ -0,0 +1,136 @@
+ function search_todo()
+  {
+
+    var todo_no = parseInt($("#search").val(),10);
+    
+    $("#todos ul li ul li").css("display","none");
+ 
+    $("#todos ul li ul li").each(function(index){
+    var len = $(this).children("div#todo_box").children().length;
+
+    if(len == (todo_no+1))
+    {
+       $(this).slideDown().siblings("li").slideDown();
+       $(this).children("div#emp_name").fadeTo(1500, 0).fadeTo(1500, 1.0);
+     }
+  
+  });
+}
+
+
+ $(document).ready(function(){
+   
+   $("#todos > ul > li > ul").delegate(" div#todo_box > img","click", function(){
+
+     var todo_div = $(this).parent();
+
+     if($(todo_div).children().length == 3)
+     $(todo_div).css("overflow","scroll");
+     
+       
+     $(todo_div).animate({ scrollTop: $(todo_div).children().last().offset().top},'slow');
+
+     $(todo_div).children("span").detach();
+
+     var todo_container = $("<div/>").attr("id","todo_container");
+     var input_text = $("<input/>").attr("text","textbox").attr("id","text").attr("size","32px");
+     var new_todo = $("<div/>").attr("id","new_todo");
+     input_text.appendTo(new_todo);
+     var img_todo = $("<div/>").attr("id","img_todo");
+
+
+     var add_img = $("<input />").attr("class","inplace_save add").attr("type","image").attr("src","images/save.png");
+     var delete_img = $("<input />").attr("class","delete inplace_cancel").attr("type","image").attr("src","images/delete.png");
+     var edit_img = $("<input />").attr("class","edit").attr("type","image").attr("src","images/edit.png");
+
+
+     add_img.appendTo(img_todo);
+     delete_img.appendTo(img_todo);
+
+     new_todo.appendTo(todo_container);
+     img_todo.appendTo(todo_container);
+
+     todo_container.appendTo(todo_div);
+
+     $("* #img_todo").delegate(".edit","click", function(){
+
+      var text = $("#new_todo").text();
+      var input = $("<input/>").attr("text","textbox").attr("id","text").attr("size","32px").attr("value",text);
+      var textbox = $(this).parent().siblings("#new_todo");
+
+      $(textbox).text("").append(input).css( "border","");
+      $(this).replaceWith(add_img);
+    });
+
+   $("* #img_todo").delegate(".add","click", function(){
+
+    var text = $("#text").val();
+    $("#text").detach();
+    var textbox = $(this).parent().siblings("#new_todo");
+
+    $(textbox).text(text).css( "border","1px solid");
+    $(this).replaceWith(edit_img);
+   });
+
+  $("* #img_todo").delegate(".delete","click", function(){
+
+    $(this).parent().parent().detach();
+
+    if($(todo_div).children().length == 1)
+    {
+      var span = $("<span/>").attr("id","add_todo").text("add todos for "+name+" here");          
+      $(todo_div).append(span);    
+    }       
+        
+  });
+
+ });
+    
+  $("#todos >ul> img").click(function(e){
+
+   var li_display = $(this).attr("id");
+
+   if(li_display == 'expand')
+    $(this).siblings("li").children().children("li").css("display","block");
+   else
+    $(this).siblings("li").children().children("li").css("display","none");
+      
+  });
+
+
+  $("#employees").delegate(".ui-draggable ","mouseover mouseout", function(event){
+  
+    var li = $(this);
+    if (event.type == 'mouseover') 
+    {
+
+      $(li).css("background-color","grey");
+      var del = $("<img />").attr("src","images/delete.png").css("height","20px").css("width","20px");
+      del.prependTo($(li));
+
+      $(li).children("img").click(function(){
+        var del_li = confirm("Do u want to delete the Employee !");
+        if(del_li == true)
+        {
+
+          var name = $(li).text();
+          var todo_div = $("#todos div#emp_name:contains('"+name+"') ");
+          var todo_li = $(todo_div).parent();
+
+          var roles_div = $("#roles ul>li>ul>li:contains('"+name+"') ");
+          var roles_li = $(roles_div).parent();
+
+
+          $(li).detach();
+          $(todo_li).detach();
+          $(roles_li).detach();
+         }
+       });     
+     }  
+   else{
+       $(this).css("background-color","white");
+       $(this).children("img").detach();
+       }
+  });
+
+});
